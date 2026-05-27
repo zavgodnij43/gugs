4955775c13317d0ddfbdedbe66b1d4be36eec0cc
zavgodnij43 <i58409353@gmail.com>
2026-05-27 15:24:07 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -16,3 +16,10 @@ class Book(Base):
     description = Column(String, index=True)
     author_id = Column(Integer, ForeignKey("author.id"))
     author = relationship("Author", back_populates="books")
+
+class User(Base):
+   __tablename__ = "users"
+   id = Column(Integer, primary_key=True, index=True)
+   login = Column(String, unique=True, index=True)
+   hashed_password = Column(String)
+   rights = Column(String, default="user")
